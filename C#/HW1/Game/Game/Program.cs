eb91ec4ddd86b9311151279f1de94a8033e19f7e
Zhugelyn <kibitkin_00@list.ru>
2023-07-30 01:58:52 +0400

    С# Домашнее задание №1

---
+++ C#/HW1/Game/Game/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var attackingUnit = new Unit(50, 80, Faction.dark, true);
+            var defendingUnit = new Unit(40, 50, Faction.light, false);
+
+            var damageDone = attackingUnit.CauseDamage(defendingUnit);
+
+            Console.WriteLine($"УРА! было нанесено {damageDone} урона");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#/HW1/Game/Game/Unit.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    internal class Unit
+    {
+        private float BaseDamage { get; set; }
+        private float BasicProtection { get; set; }
+        private Faction Faction { get; set; }
+        private bool IsBerserk { get; set; }  
+
+        public Unit (uint baseDamage, uint basicProtection, Faction faction, bool isBerserk)
+        {
+            this.BaseDamage = baseDamage; 
+            this.BasicProtection = basicProtection;
+            this.Faction = faction;
+            this.IsBerserk = isBerserk;
+        }
+
+        public float CauseDamage(Unit defendingUnit)
+        {
+            return СalculateTotalIncomingDamage(this, defendingUnit);
+        }
+
+        private float СalculateTotalIncomingDamage(Unit attackingUnit, Unit defendingUnit)
+        {
+            var damage = CalculateDamage(attackingUnit.BaseDamage, attackingUnit.IsBerserk) *
+                DetermineFactionBonusDamageRatio(attackingUnit.Faction, defendingUnit.Faction);
+            var protection = CalculateProtection(defendingUnit.BasicProtection, defendingUnit.IsBerserk);
+
+            return damage * protection;
+        }
+
+        private float DetermineFactionBonusDamageRatio(Faction factionAttackingUnit, Faction defendingUnitFaction)
+        {
+            if (factionAttackingUnit == Faction.gray)
+                return 0.00f;
+            if (factionAttackingUnit == defendingUnitFaction)
+                return 0.5f;
+            if (factionAttackingUnit != defendingUnitFaction)
+                return 2;
+
+            return 0.00f;
+        }
+
+        private float CalculateDamage(float baseDamage, bool isBerserk)
+        {
+            return baseDamage = isBerserk ? baseDamage * 2 : baseDamage;
+        }
+
+        private float CalculateProtection(float basicProtection, bool isBerserk)
+        {
+            basicProtection = isBerserk ? (basicProtection * 0.2f) : basicProtection;
+            return (100 - basicProtection) / 100;
+        }
+
+    }
+}
